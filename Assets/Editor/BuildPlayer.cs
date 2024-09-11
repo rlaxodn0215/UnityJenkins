a484951debbd4d624d594ea9e9733139853d10a2
rlaxodn <rlaxodn0215@naver.com>
2024-09-11 16:17:54 +0900

    20240911

--- Assets/Editor/BuildPlayer.cs
+++ Assets/Editor/BuildPlayer.cs
@@ -8,11 +8,46 @@ using UnityEditor.Build.Reporting;
 
 public class BuildPlayer : MonoBehaviour
 {
+    [MenuItem("Build/StandaloneWindows")]
+    public static void MyBuild_StandaloneWindows()
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        List<string> scenes = new List<string>();
+        foreach (var scene in EditorBuildSettings.scenes)
+        {
+            if (!scene.enabled) continue;
+            scenes.Add(scene.path);
+        }
+        buildPlayerOptions.scenes = scenes.ToArray();
+        buildPlayerOptions.locationPathName = "Builds/UnityJenkins.exe";
+        buildPlayerOptions.target = BuildTarget.StandaloneWindows;
+        buildPlayerOptions.options = BuildOptions.None;
+
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build failed");
+        }
+    }
+
     [MenuItem("Build/Build AOS")]
     public static void MyBuild_AOS()
     {
         BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
-        buildPlayerOptions.scenes = new[] { "Assets/MainMenu.unity", "Assets/PlayScene.unity", "Assets/GameOver.unity" };
+        List<string> scenes = new List<string>();
+        foreach (var scene in EditorBuildSettings.scenes)
+        {
+            if (!scene.enabled) continue;
+            scenes.Add(scene.path);
+        }
+        buildPlayerOptions.scenes = scenes.ToArray();
         buildPlayerOptions.locationPathName = $"Builds/AOS_{PlayerSettings.bundleVersion}.apk";
         buildPlayerOptions.target = BuildTarget.Android;
         buildPlayerOptions.options = BuildOptions.None;
